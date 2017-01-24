0b627498a84f4910611100e08b1ce0f3d4a1d004
TriptSharma <tript35.sharma@gmail.com>
2017-01-24 15:25:27 +0530

    signUp and login update1.0

--- app/src/main/java/com/halanx/tript/userapp/login.java
+++ app/src/main/java/com/halanx/tript/userapp/login.java
@@ -1,20 +1,38 @@
 package com.halanx.tript.userapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
     Button login, signup;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth= FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d("SIGNIN_ACT", "onAuthStateChanged:signed_in:"+user.getUid()); //user signed in
+                }
+                else Log.d("SIGNIN_ACT", "onAuthStatteChanged:signed_out");
+            }
+        };
         login = (Button) findViewById(R.id.button);
         signup= (Button) findViewById(R.id.sign_up);
 
@@ -51,5 +69,14 @@ public class login extends AppCompatActivity {
                 startActivity(new Intent(login.this,Call_Popups.class));
             }
         });
+    }
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null)
+        mAuth.removeAuthStateListener(mAuthListener);
     }
 }
--- app/src/main/java/com/halanx/tript/userapp/signup.java
+++ app/src/main/java/com/halanx/tript/userapp/signup.java
@@ -1,26 +1,166 @@
 package com.halanx.tript.userapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class signup extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText email_id1,password1;
+    private static final String TAG = "EmailPassword";
+    Button sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Button finish;
-        finish = (Button) findViewById(R.id.finish);
-        finish.setOnClickListener(new View.OnClickListener() {
+        email_id1 = (EditText) findViewById(R.id.email_id1);
+        password1 = (EditText) findViewById(R.id.password1);
+
+        sign_up = (Button) findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent a = new Intent(signup.this, login.class);
-                startActivity(a);
-            }
+                createAccount(email_id1.getText().toString(), password1.getText().toString());
+                }
         });
+
+        // Views
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    // [END on_stop_remove_listener]
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(signup.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(signup.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else startActivity(new Intent(signup.this,Home.class));
+
+                        /*// [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }*/
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    /*private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }*/
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = email_id1.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            email_id1.setError("Required.");
+            valid = false;
+        } else {
+            email_id1.setError(null);
+        }
+
+        String password = password1.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            password1.setError("Required.");
+            valid = false;
+        } else {
+            password1.setError(null);
+        }
+
+        return valid;
     }
 }
